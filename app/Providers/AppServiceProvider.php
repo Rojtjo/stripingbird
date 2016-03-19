f1ea0a256fae93e275e5a606cb49f12d7c666592
Roj Vroemen <me@rojvroemen.com>
2016-03-19 02:17:52 +0100

    Configure Moneybird

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use App\InvoiceRepository;
 use Illuminate\Support\ServiceProvider;
+use Moneybird\ApiConnector;
+use Moneybird\HttpClient;
+use Moneybird\XmlMapper;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +27,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(ApiConnector::class, function ($app) {
+            $config = [
+                'client' => env('MONEYBIRD_CLIENT'),
+                'email' => env('MONEYBIRD_EMAIL'),
+                'password' => env('MONEYBIRD_PASSWORD'),
+            ];
+
+            $transport = new HttpClient();
+            $transport->setAuth(
+                $config['email'],
+                $config['password']
+            );
+
+            return new ApiConnector(
+                $config['client'],
+                $transport,
+                new XmlMapper()
+            );
+        });
+
+        $this->app->singleton(InvoiceRepository::class, function($app) {
+            $connector = $app[ApiConnector::class];
+            $invoiceService = $connector->getService('Invoice');
+
+            return new InvoiceRepository($invoiceService);
+        });
     }
 }
